1c9112aa5cfdabd34f4a5369bad5f0a77a1b12e0
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-28 16:34:25 +0100

    feat: ✨ find duplicate number

--- find_duplicate_number/find_duplicate.py
+++ find_duplicate_number/find_duplicate.py
@@ -1,2 +1,23 @@
 def find_duplicate(nums):
-    pass
+
+    # bad approach
+    # for num in nums:
+    #     found = 0
+    #     for num2 in nums:
+    #         if num == num2:
+    #             found += 1
+    #         if found == 2:
+    #             return num
+
+    print(nums)
+    nums.sort()
+
+    for i, num in enumerate(nums):
+
+        if num == nums[i+1]:
+            return num
+
+    print(nums)
+
+
+print(find_duplicate([2, 3, 1, 3, 4]))
